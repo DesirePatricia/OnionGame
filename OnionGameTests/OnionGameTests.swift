839bfb8c30b686d4ec5df2c348427800852e4197
Desire <desire_guilarte@hotmail.com>
2019-05-31 12:13:10 -0400

    Initial Commit

---
+++ OnionGameTests/OnionGameTests.swift
@@ -0,0 +1,34 @@
+//
+//  OnionGameTests.swift
+//  OnionGameTests
+//
+//  Created by Desire Guilarte on 2019-05-31.
+//  Copyright © 2019 DesGui. All rights reserved.
+//
+
+import XCTest
+@testable import OnionGame
+
+class OnionGameTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
